1a10beda4fba72998c89c704ce691f2f9f20c084
Bhavya Modi <70645123+bhavyamodi@users.noreply.github.com>
2024-01-29 12:57:44 +0530

    Delete array directory

--- array/allSubsetOfArray.js
+++
@@ -1,8 +0,0 @@
-const getAllSubsets = theArray => theArray.reduce(
-        (subsets, value) => subsets.concat(
-         subsets.map(set => [value,...set])
-        ),
-        [[]]
-      );
-
-console.log(getAllSubsets([1,2,3]));
